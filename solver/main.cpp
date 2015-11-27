dd91843a2e472dc5421db11c3add986ccd7b2f29
dzikakulka <tofix112@gmail.com>
2015-11-27 22:44:06 +0100

    interfejs rozwiazywania z palcow

--- solver/main.cpp
+++ solver/main.cpp
@@ -3,32 +3,9 @@
 
 int main()
 {
-	/*Vector v1(4,1);
-    Vector v2 = v1;
-	v1.Print();	v2.Print();
-	v1.Read();
-	v1.Print();	v2.Print();*/
 
-	/*
 	int rozm;
-	std::cout << "Rozmiar: "; std::cin >> rozm;
-	
-	Matrix m1(rozm, rozm);
-	
-	Vector *rows = new Vector[rozm];
-	std::cout << std::endl;
-	for(int i=0; i<rozm; i++){
-		rows[i].Resize(rozm);
-		rows[i].Read();
-		m1.Set(rows[i], i);
-	}
-	m1.Print();
-	std::cout << "DET: " << m1.Det();
-	m1.Print();
-	*/
-
-		int rozm;
-	std::cout << "Rozmiar: "; std::cin >> rozm;
+	std::cin >> rozm;
 	
 	Matrix m1(rozm, rozm);
 	Vector v1(rozm);
@@ -43,18 +20,14 @@ int main()
 	v1.Read();
 
 	EqSys s1(m1, v1);
-	s1.Print();
-	Vector rozwC, rozwG;
-	s1.Solve_Cram(rozwC);
-	s1.Solve_Gauss(rozwG);
-	Nl();
-	M("Rozw Cramr: "); rozwC.PrintIl(); Nl();	
-	M("Rozw Gauss: "); rozwG.PrintIl(); Nl();
-/*m1.Print();
-	m1.Resize(3, 5);
-	m1.Print();
-	Matrix m2(m1.Transpose());
-	m2.Print();*/
-	
+	Vector rozwC, rozwG, bladC, bladG;
+	s1.Solve_Cram(rozwC, bladC);
+	s1.Solve_Gauss(rozwG, bladG);
+	M("#C");
+	rozwC.Print();
+	bladC.Print(); Nl(2);
+	M("#G");
+	rozwG.Print();
+	bladG.Print();
 	return 0;
 }
